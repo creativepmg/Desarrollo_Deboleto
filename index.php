3af00e0f9dbcc5c2a98a8e5e64d0660c2c3ca12a
burngeek8 <burngeek8@gmail.com>
2014-01-14 11:08:33 -0500

    Primer Commit despues de ayudar a phillip

---
+++ index.php
@@ -0,0 +1,185 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta property=og:image content="imagenes/logo.png"/>
+	<meta name="description" content="Radio Online donde encontraras lo mejor de la musica">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<meta name="HandheldFriendly" content="true" />
+
+	<title>Radio Deboleto</title>
+
+	<link href='http://fonts.googleapis.com/css?family=Romanesco' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Pompiere' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Cantora+One' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Pompiere' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Text+Me+One' rel='stylesheet' type='text/css'>
+	<LINK REL="Shortcut Icon" HREF="imagenes/logo.ico" />
+	<link rel="image_src" href="imagenes/logo.png" />
+	<link rel="stylesheet" href="utilidades/normalize.css" />
+	<link rel="stylesheet" href="css/estilo.css" />
+	<!-- Insert to your webpage before the </head> -->
+    <script src="slider/java/jquery.js"></script>
+    <script src="slider/java/amazingslider.js"></script>
+    <script src="slider/java/initslider-1.js"></script>
+    <!-- End of head section HTML codes -->
+
+	<!-- <script type="text/javascript">
+document.oncontextmenu = function(){return false;}
+</script> -->
+<script id="_wau188">var _wau = _wau || [];
+_wau.push(["tab", "tek4ntnrarb5", "188", "left-middle"]);
+(function() {var s=document.createElement("script"); s.async=true;
+s.src="http://widgets.amung.us/tab.js";
+document.getElementsByTagName("head")[0].appendChild(s);
+})();</script>
+</head>
+<body>
+	<div id="cuerpo">
+		<header>
+			<div class="logo">
+				<a href="#"><img src="imagenes/logo.png" alt=""></a>
+			</div>
+			<div class="titulo">
+				<h1>Radio Deboleto | Bienvenidos</h1>
+			</div>
+			<div class="redes">
+				<a href="https://www.facebook.com/RadioDeboletoo" target="_blank"><img src="imagenes/fb.png" alt="facebook" title="Facebook/deboleto"></a>
+				<a href="https://twitter.com/DeboletoPe" target="_blank"><img src="imagenes/twitter.png" alt="twitter" title="twitter/deboleto"></a>
+				<a href="http://www.youtube.com/user/DeboletoTV" target="_blank"><img src="imagenes/youtube.png" alt="youtube" title="youtube/deboleto"></a>
+			</div>
+		</header>
+		<div class="cabezal">
+			<div class="frase">
+				<h2>Feliz Navidad y Un Prospero Año Nuevo </br>Te Desea Deboleto.Pe! - Estamos A otro Level</h2>
+			</div>
+		</div>
+		<div class="line1">
+			<img src="imagenes/medio3.png">
+		</div>
+		<div class="celular">
+			<div class="tituloline2">
+				<h3>Escucha la radio en tu celular:</h3>
+			</div><br>
+			<div class="line2">
+				<a href="rtsp://aac.01wzserver.com:1935/aacplus/8002" target="_blank"><img src="imagenes/android.png" alt="escuchanosenandroid"></a>
+				<a href="http://aac.01wzserver.com:1935/aacplus/8002/playlist.m3u8" target="_blank"><img src="imagenes/apple.png" alt="escuchanosenapple"></a>
+				<a href="rtsp://aac.01wzserver.com:1935/aacplus/8002" target="_blank"><img src="imagenes/bb.png" alt="escuchanosenblacberry"></a>
+			</div>
+		</div>
+		<div class="youtube">
+			<HR width=99% align="left" noshade="noshade" style="color: #FFFFFF;">
+			<h4>
+				Si quieres ver nuestros videos...Pulsa en el Logo de Youtube. GRACIAS!
+			</h4>
+		</div>
+	</div>
+	<div id="cuerpo2">
+		<div class="chat">
+			<iframe src="http://chatwing.com/deboletopee">Tu Dispositivo no soporta  La caja de flash o Iframes ! gracias</iframe>
+		</div>
+		<div class="reprolike">
+			<div class="repro">
+				<embed pluginspage="http://www.adobe.com/go/getflashplayer"src="repro/reprodeboletos.swf" width="302" height="160" wmode="transparent"type="application/x-shockwave-flash" allowscriptaccess="always"></embed>
+			</div>
+			<div class="like">
+				<iframe src="http://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FRadioDeboletoo&width&height=258&colorscheme=light&show_faces=true&header=false&stream=false&show_border=false&appId=334775653285723" frameborder="0">Tu Dispositivo no soporta  La caja de flash o Iframes ! gracias</iframe>
+			</div>
+		</div>
+		<div class="videos">
+
+			<div class="ddc">
+			<div class="tittleddc">
+				<p>Detras de<b> CÁMARAS</b></p>
+			</div>
+				  <!-- Insert to your webpage where you want to display the slider -->
+    			<div id="amazingslider-1" style="display:block;position:relative;margin:0px auto 			0px;">
+        			<ul class="amazingslider-slides" style="display:none;">
+           			 <li><img src="slider/imagenes/imagen1.png" alt="imagen1" /></li>
+           			 <li><img src="slider/imagenes/imagen2.png" alt="imagen2" /></li>
+           			 <li><img src="slider/imagenes/imagen3.png" alt="imagen3" /></li>
+           			 <li><img src="slider/imagenes/imagen4.png" alt="imagen4" /></li>
+          			  <li><img src="slider/imagenes/imagen5.png" alt="imagen5" /></li>
+       			 </ul>
+       			 <div class="amazingslider-engine" style="display:none;"><a href="http://			amazingslider.com">jQuery Slideshow</a></div>
+   			 	</div>
+    			<!-- End of body section HTML codes -->
+			</div>
+			<div class="ev">
+				<div class="tittleev">
+				<p>Estreno de<b> VIDEO</b></p>
+				</div>
+				<iframe src="https://youtube.googleapis.com/v/wTsTKGzKm80?hl=es_MX" frameborder="0"></iframe>
+			</div>
+
+			<div class="pe">
+				<div class="tittleep">
+				<p>Proximo<b> ESTRENO</b></p>
+				</div>
+				<img src="imagenes/proximamente.jpg" alt="">
+			</div>
+		</div>
+		<div class="publicidad">
+			<div class="tittleddc">
+				<p>Pon aqui tu<b> PUBLICIDAD</b></p>
+			</div>
+			<div class="publicidad1">
+				<img src="imagenes/publicidad/publicidad1.jpg" alt="publicidad1">
+			</div>
+			<div class="publicidad2">
+				<img src="imagenes/publicidad/publicidad2.jpg" alt="publicidad2">
+			</div>
+		</div>
+		<div class="noticias">
+						
+			<?php
+				include("conexion.php");
+
+				$con = mysql_connect($host,$user,$pw)
+				or die("problemas al conectar con el servidor");
+
+				mysql_select_db($db,$con)
+				or die("problemas al conectar db");
+
+				$registro = mysql_query("SELECT * FROM noticias ORDER BY FECHA DESC LIMIT 3")
+				or die("problemas en consulta:".mysql_error());
+
+				while ($reg=mysql_fetch_array($registro)) {
+					echo "<div class='noticia'>";
+					echo "<div class='imagennoticia'>";
+					echo "<a href='".$reg['LINK']."' target='_blank' >";
+					echo "<img src='".$reg['IMAGEN']."' >";
+					echo "</a>";
+					echo "</div>";
+					echo "<div class='titulonoticia'>";
+					echo "<a href='".$reg['LINK']."' target='_blank' >";
+					echo "<p class='usuario'><b>".$reg['TITULO']."</b></p>";
+					echo "</a>";
+					echo "</div>";
+					echo "<div class='fecha'>";
+					echo "<p class='color'>".$reg['FECHA']."</p>";
+					echo "</div>";			
+					echo "</div>";				
+			}
+			?>
+		</div>
+	</div>	
+	<footer>
+		<div class="contacto">
+			<b>Contáctanos</b>
+			 <p>Contacto@deboleto.pe</p>
+			 <p>Telf: ***-****</p>
+			 <p>phone : 991844695</p>
+		</div>
+		<div class="logofinal1">
+			<img src="imagenes/logo2.png" alt="logo2">
+		</div>
+		<div class="logofinal2">
+			<img src="imagenes/finallogo.png" alt="deboleto.pe">
+		</div>
+		<div class="derechos">
+			<p>© 2013 Todos los derechos Reservados por Deboleto.pe</p>
+		</div>
+	</footer>
+</body>
+</html>
